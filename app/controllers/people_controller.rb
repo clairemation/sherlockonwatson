757a2fc715790f215db8c243238e68c6b63d734d
dmandelb <devin.mandelbaum@gmail.com>
2016-06-25 15:54:00 -0400

    Put together two separate pieces to get working product

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,21 +1,22 @@
 class PeopleController < ApplicationController
   def index
-    @people = Person.all
+    @people = Person.all.order(:id)
     @person = Person.new
   end
 
   def create
-    @result = Person.find_by(submit_params)
-    if @result
-      render :success
+    # binding.pry
+    if params[:images]["0"]["classifiers"]
+      @result = Person.find_by(name: params[:images]["0"]["classifiers"]["0"]["classes"]["0"]["class"])
+      render :success, layout: false
     else
-      render :failure
+      render :failure, layout: false
     end
     # render json: @result.to_json
   end
 
-  private
-  def submit_params
-    params.require(:person).permit(:name)
-  end
+  # private
+  # def submit_params
+  #   params.require(:person).permit(:name)
+  # end
 end

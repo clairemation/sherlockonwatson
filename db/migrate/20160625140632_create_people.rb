d9adebb9c22f3f323ed17abb3c2c3a71aa989036
dmandelb <devin.mandelbaum@gmail.com>
2016-06-25 10:57:11 -0400

    Add people objects and resources, with index page and display of all people

--- db/migrate/20160625140632_create_people.rb
+++ db/migrate/20160625140632_create_people.rb
@@ -1,7 +1,8 @@
 class CreatePeople < ActiveRecord::Migration
   def change
     create_table :people do |t|
-
+      t.string :name, null: false
+      t.text :image_url, null: false
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625033112) do
+ActiveRecord::Schema.define(version: 20160625140632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "reports", force: :cascade do |t|
+  create_table "people", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.text     "image_url",  null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.string   "api_key"
+    t.string   "classifier_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
